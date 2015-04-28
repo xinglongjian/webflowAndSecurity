4288f4c9ae234f1af0a76ef4ef256562c1742957
xinglongjian <xinglongjian@qq.com>
2015-04-29 06:36:54 +0800

    add converter

--- src/main/java/com/xinglongjian/config/WebMvcContextConfiguration.java
+++ src/main/java/com/xinglongjian/config/WebMvcContextConfiguration.java
@@ -1,10 +1,16 @@
 package com.xinglongjian.config;
 
+import org.springframework.binding.convert.converters.StringToEnum;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.DateFormatter;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.xinglongjian.converter.StringToEntityConverter;
+import com.xinglongjian.domain.Category;
 /**
  * web mvc config
  * @author root
@@ -22,5 +28,20 @@ public class WebMvcContextConfiguration extends WebMvcConfigurationSupport {
 		 viewResolver.setSuffix(".jsp");
 		 return viewResolver;
 	}
+	
+	@Bean
+	public StringToEntityConverter categoryConverter()
+	{
+		return new StringToEntityConverter(Category.class);
+	}
+
+	@Override
+	protected void addFormatters(FormatterRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addConverter(categoryConverter());
+		registry.addFormatter(new DateFormatter("dd-MM-yyyy"));
+	}
+	
+	
 	
 }
---
+++ src/main/java/com/xinglongjian/converter/StringToEntityConverter.java
@@ -0,0 +1,71 @@
+package com.xinglongjian.converter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.springframework.core.convert.TypeDescriptor;
+import org.springframework.core.convert.converter.GenericConverter;
+
+/**
+ * 
+ * @author root
+ *
+ */
+public class StringToEntityConverter implements GenericConverter {
+
+	private static final String ID_FIELD = "id";
+	private final Class<?> clazz;
+	
+	public StringToEntityConverter(Class<?> clazz) {
+		 super();
+		 this.clazz = clazz;
+	}
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	
+	@Override
+	public Set<ConvertiblePair> getConvertibleTypes() {
+		// TODO Auto-generated method stub
+		Set<ConvertiblePair> types = new HashSet<GenericConverter.ConvertiblePair>();
+		 types.add(new ConvertiblePair(String.class, this.clazz));
+		 types.add(new ConvertiblePair(this.clazz, String.class));
+		 return types;
+	}
+
+	@Override
+	public Object convert(Object source, TypeDescriptor sourceType,
+			TypeDescriptor targetType) {
+		// TODO Auto-generated method stub
+		if(String.class.equals(sourceType.getType()))
+		{
+			if(StringUtils.isBlank((String)source)){
+				return null;
+			}
+			Long id=Long.parseLong((String)source);	
+			return this.em.find(this.clazz, id);
+		}
+		else if(this.clazz.equals(sourceType.getType()))
+		{
+			try {
+				if(source==null)
+				{
+					return "";
+				}
+				else{
+					return FieldUtils.readField(source, ID_FIELD,true).toString();
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		throw new IllegalArgumentException("Cannot convert " + source + " into a suitabletype!");
+	}
+
+}
